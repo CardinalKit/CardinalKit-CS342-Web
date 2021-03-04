f676b50b36cdb6884e44c16e5c60b624abb29ebf
Collin Schlager <schlagercollin@gmail.com>
2021-03-03 21:41:53 -0800

    Reconfigure provider check based on new firebase forma.

--- ReactJS-Project-CardinalKit/src/api/admin.ts
+++ ReactJS-Project-CardinalKit/src/api/admin.ts
@@ -1,16 +1,22 @@
 import app from 'firebase/app';
 import Firebase from '../components/Firebase';
 
+// checks 
 export function isAdmin(userEmail: string): Promise<boolean> {
   const firebase = new Firebase();
-  var adminBool = firebase.db.collection('providers').doc('providers').get()
-  .then(function(doc) {
-    var data = doc.data();
-    return (data && data["providers"].includes(userEmail));
-  })
-  .catch(function(error) {
-    console.log('Error getting document:', error);
-    return false;
-  });
+  var adminBool = firebase.db.collection('providers').get()
+    .then(function (querySnapshot) {
+      var result = false;
+      querySnapshot.forEach(function(doc) {
+        if (doc.id == userEmail && doc.data()["admin"]) {
+          result = true;
+        }
+      })
+      return result;
+    })
+    .catch(function (error) {
+      console.log('Error getting document:', error);
+      return false;
+    });
   return adminBool;
 }
