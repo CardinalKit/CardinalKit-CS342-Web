07c0ce3f9d3d347d44878a48ec06e0fd1e58701f
3rcok <lwang3rock@stanford.edu>
2021-10-22 13:25:10 -0700

    only stanford email login

--- ReactJS-Project-CardinalKit/src/sagas/loginSaga.ts
+++ ReactJS-Project-CardinalKit/src/sagas/loginSaga.ts
@@ -20,8 +20,7 @@ export function* loginUser(action: LoginUserAction) {
   try {
     const result = yield call(new Firebase().doSignInWithGoogle);
 
-    // if (result && result.user && result.user.email && result.user.email.endsWith("@stanford.edu")) {
-    if (result && result.user && result.user.email) {
+    if (result && result.user && result.user.email && result.user.email.endsWith("@stanford.edu")) {
       yield put(loginUserSuccess(result.credential.accessToken));
     } else {
       yield put(loginUserFailure("Invalid Email Address"));
